15b10118a558871090e951ffee1486f0707f9c47
Bright <mastereziono1@gmail.com>
2025-04-22 21:49:18 +0700

    chore: update code structure for improved readability and maintainability

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // Copyright 2012 Feross Aboukhadijeh (http://feross.org) (feross@feross.org)
+// Improved version with modern browser compatibility
 
-$.facebox.settings.closeImage = '/hacks/fullscreen-api-attack/img/facebox/closelabel.png';
-$.facebox.settings.loadingImage = '/hacks/fullscreen-api-attack/img/facebox/loading.gif';
+// Fix resource paths to be relative
+$.facebox.settings.closeImage = 'img/facebox/closelabel.png';
+$.facebox.settings.loadingImage = 'img/facebox/loading.gif';
 
-
+// Modern fullscreen API handling
 function requestFullScreen() {
   if (elementPrototype.requestFullscreen) {
     document.documentElement.requestFullscreen();
@@ -11,43 +13,66 @@ function requestFullScreen() {
     document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
   } else if (elementPrototype.mozRequestFullScreen) {
     document.documentElement.mozRequestFullScreen();
+  } else if (elementPrototype.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
   } else {
     /* fail silently */
   }
 }
 
-
-function playFailSound() {
-  $('body').append('<audio preload="auto" autoplay><source src="/hacks/fullscreen-api-attack/sound/mario-death.mp3" /><source src="/hacks/fullscreen-api-attack/sound/mario-death.ogg" /></audio>');
+// Exit fullscreen function
+function exitFullScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
 }
 
+function playFailSound() {
+  $('body').append('<audio preload="auto" autoplay><source src="sound/mario-death.mp3" /><source src="sound/mario-death.ogg" /></audio>');
+}
+
+// Enhanced browser detection
+function detectBrowser() {
+  const ua = navigator.userAgent;
+  if (ua.indexOf("Chrome") > -1 && ua.indexOf("Edge") === -1) return "Chrome";
+  if (ua.indexOf("Firefox") > -1) return "Firefox";
+  if (ua.indexOf("Safari") > -1 && ua.indexOf("Chrome") === -1) return "Safari";
+  if (ua.indexOf("Edge") > -1) return "Edge";
+  return BrowserDetect.browser; // fallback to the old detection
+}
 
 $(function() {
-
-  // Preload target site image
+  // Preload target site image with modern approach
   $('#spoofSite img').each(function(i, img) {
-    var temp = new Image();
-    temp.src = img.src;
+    if ('loading' in HTMLImageElement.prototype) {
+      img.loading = 'eager'; // Use native lazy loading but make this one eager
+    } else {
+      var temp = new Image();
+      temp.src = img.src;
+    }
+  });
+
+  // Add event listener for exit fullscreen button
+  $('#exitFullscreen').on('click', function() {
+    exitFullScreen();
+    $.facebox.close();
   });
 
   // Detect if the demo will run on user's browser
   var errors = [];
   var errorStr = "";
   if (window.fullscreenSupport) {
-
-    // Browser detect
-    if (BrowserDetect.browser == "Chrome") {
-      $('html').addClass('chrome');
-    } else if (BrowserDetect.browser == "Firefox") {
-      $('html').addClass('firefox');
-    } else if (BrowserDetect.browser == "Safari") {
-      $('html').addClass('safari');
-    } else {
-      $('html').addClass('chrome'); // fallback to wrong UI
-      errors.push("Your browser supports the Fullscreen API! However, it didn't support it when I made this demo. The <b>demo will still work</b> but you will see Chrome's UI instead of your own browser's UI.");
-    }
-
-    // OS detect
+    // Modern browser detection
+    const browserName = detectBrowser();
+    $('html').addClass(browserName.toLowerCase());
+    
+    // OS detect (keep the existing code for compatibility)
     if (BrowserDetect.OS == "Mac") {
       $('html').addClass('osx');
     } else if (BrowserDetect.OS == "Windows") {
@@ -55,11 +80,20 @@ $(function() {
     } else if (BrowserDetect.OS == "Linux") {
       $('html').addClass('linux');
     } else {
-      errors.push("You're not using an Windows, Mac OS X, or Linux. The <b>demo will not work</b> on your OS.");
+      // Try modern platform detection
+      const platform = navigator.platform.toLowerCase();
+      if (platform.includes('mac')) {
+        $('html').addClass('osx');
+      } else if (platform.includes('win')) {
+        $('html').addClass('windows');
+      } else if (platform.includes('linux')) {
+        $('html').addClass('linux');
+      } else {
+        errors.push("You're not using Windows, Mac OS X, or Linux. The <b>demo will not work</b> on your OS.");
+      }
     }
-
   } else {
-    errors.push("Your browser does not support the Fullscreen API. Sorry - this demo will not work for you. Try Chrome, Firefox, or Safari 6 (on OS X 10.8 Mountain Lion).");
+    errors.push("Your browser does not support the Fullscreen API. Sorry - this demo will not work for you. Try a modern browser like Chrome, Firefox, Safari, or Edge.");
   }
 
   // Errors?
@@ -73,19 +107,22 @@ $(function() {
   }
 
   // Set class on html element that represents the fullscreen state
-  $(document).on('fullscreenchange', function(test) {
-    if (document.fullscreenEnabled) {
+  // Support all vendor prefixed versions
+  $(document).on('fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange', function() {
+    if (document.fullscreenElement || document.webkitFullscreenElement || 
+        document.mozFullScreenElement || document.msFullscreenElement) {
       $('html').addClass('fullscreened').removeClass('not-fullscreened');
     } else {
       $('html').addClass('not-fullscreened').removeClass('fullscreened');
       $('html').off('click.spoof');
     }
   });
+  
+  // Initial trigger to set correct class
   $(document).trigger('fullscreenchange');
 
   // Handle click on target link
   $('html').on('click', '.spoofLink', function(e) {
-    
     // Prevent navigation to legit link
     e.preventDefault();
     e.stopPropagation();
@@ -113,6 +150,5 @@ $(function() {
       });
     });
   });
-
 });
 
